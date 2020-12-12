e8456c915356f7e1ca5f81d2c70b534494b94b6c
Jay Jung <jayjung@ezion.com.au>
2020-12-12 14:20:47 +1100

    # First commit with default packages, structure and settings
    - Use dotenv for define env
    - Use jest for BDD/TDD
    - Use nodemon for server start
    - Use TypeORM for database ORM

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,24 @@
+import * as dotenv from 'dotenv';
 import * as express from 'express';
+import * as cors from 'cors';
+import * as bodyParser from 'body-parser';
+
+// initialize configs
+dotenv.config();
+
 
 const server = express();
-const port = 8080;
+const env = process.env.NODE_ENV;
+const port = process.env.SERVER_PORT || 8080;
+
+
+// Adding middleware to parse all incoming requests as JSON
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: false}));
+// Adding middleware to allow cross-origin requests
+server.use(cors());
+
 
 server.listen(port, () => {
-    console.log(`Server is listening at http://localhost:${port}`);
+    console.log(`[${env}] Server is listening at http://localhost:${port}`);
 });
